433bfb511a3c097e276c7fe4c2b037e5e955025b
Vcaze <viktor.nordenberg1@gmail.com>
2024-09-26 04:26:32 +0200

    no change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 // src/components/Header.js
 
 import React from "react";
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
@@ -10,6 +11,10 @@ export default function Header() {
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
+
+            {/* det kanske ska göras på sättet nedan istället för ovan. Nedan ska vara för SPA, vet inte om ovan är det eller ej */}
+            {/* <NavLink to="/new">Add Event</NavLink> */}
+
         </nav>
     )
 }
