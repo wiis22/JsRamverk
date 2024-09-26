3ed58a4c0b7d7e7ce3a5cb7c7a1f35bd78a1463c
Vcaze <viktor.nordenberg1@gmail.com>
2024-09-26 04:26:10 +0200

    tried changing redirecting method to keep SPA

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,11 @@
 // src/pages/Home.js
 
 import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 // import { useParams } from 'react-router-dom';
 
 export default function Home() {
+    let history = useHistory();
 
     const [newTitle, setNewTitle] = useState('');
     const [docs, setDocs] = useState([]);
@@ -25,7 +27,7 @@ export default function Home() {
             title: newTitle
         };
         try {
-            const response = await fetch('http://localhost:4000/api/new-doc', { // använder inte response nu men kan vara bra att göra en try catch
+            const response = await fetch('http://localhost:4000/api/new-doc', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -37,10 +39,12 @@ export default function Home() {
                 throw new Error(`HTTP error, status ${response.status}`);
             }
 
-            const result = await response.json();
-            // console.log(result);
-            //redirect to rout /doc/{result} cuse you will get a id as response from new-doc if succes.
-            window.location.href = `/doc/${result}`;
+            const newDocId = await response.json();
+
+            console.log("redirecting to /doc/" + newDocId);
+            // return redirect(`/doc/${newDocId}`);
+
+            history.push(`/doc/${newDocId}`) // denna redirect fungerar ej. problemet är att history får tyldigen inte skapas pga att det inte verkar vara inuti en routing ellet nåt. har testat flera andra redirects. den gamla var inte SPA.
         } catch (err) {
             console.error("Fetch error:", err)
         }
@@ -56,7 +60,7 @@ export default function Home() {
                     ) : (
                         docs.map((doc) => (
                             <div className='doc-in-list' key={doc._id}>
-                                <a href={`/doc/${doc._id}`}>{doc.title}</a>
+                                <a href={`/doc/${doc._id}`}>{doc.title} - {doc._id}</a>
                             </div>
                             ))
                     )}
